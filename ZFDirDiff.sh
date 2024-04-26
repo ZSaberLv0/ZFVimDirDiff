016c576f70a3cc2bfd660e8136bd674cd2573746
Uthpala Herath <uthpala.herath@duke.edu>
2024-04-25 23:12:43 -0400

    Updated ZFDirDiff.sh to work with both absolute and relative paths.

--- ZFDirDiff.sh
+++ ZFDirDiff.sh
@@ -1,6 +1,19 @@
 WORK_DIR=$(cd "$(dirname "$0")"; pwd)
-PATH_A=$1
-PATH_B=$2
+
+# Check if absolute path is given for PATH_A
+if [[ "$1" = /* ]]; then
+    PATH_A=$1
+else
+    PATH_A=$(pwd)/$1
+fi
+
+# Check if absolute path is given for PATH_B
+if [[ "$2" = /* ]]; then
+    PATH_B=$2
+else
+    PATH_B=$(pwd)/$2
+fi
+
 if test "0" = "1" \
     || test "x-$PATH_A" = "x-" \
     || test "x-$PATH_B" = "x-" \
@@ -15,4 +28,3 @@ if test "x-$ZFDIRDIFF_VIM" = "x-"; then
 fi
 
 "$ZFDIRDIFF_VIM" -c "call ZFDirDiff(\"$PATH_A\", \"$PATH_B\")"
-
